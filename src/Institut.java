91a7ce9acee6adbf1eaa0c84109d127d92d60462
David <dsantacru@institutjaumehuguet.cat>
2023-03-10 20:08:18 +0100

    Ejercicio terminado

--- src/Institut.java
+++ src/Institut.java
@@ -1,3 +1,51 @@
 public class Institut {
-    
+    private String nom;
+    private Alumne[] llistaAlumnes;
+    private int num;
+
+    public Institut(String nom) {
+        this.nom = nom;
+        this.llistaAlumnes = new Alumne[100];
+        this.num = 0;
+    }
+
+    public Institut(String nom, int maxAlumnes) {
+        this.nom = nom;
+        this.llistaAlumnes = new Alumne[maxAlumnes];
+        this.num = 0;
+    }
+
+    public void afegirAlumne(Alumne al) {
+        if (num < llistaAlumnes.length) {
+            llistaAlumnes[num] = al;
+            num++;
+        } else {
+            System.out.println("La llista d'alumnes esta plena.");
+        }
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Alumne[] getLlistaAlumnes() {
+        return llistaAlumnes;
+    }
+
+    public void setLlistaAlumnes(Alumne[] llistaAlumnes) {
+        this.llistaAlumnes = llistaAlumnes;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
--- src/UsaInstitut.java
+++ src/UsaInstitut.java
@@ -1,3 +1,32 @@
 public class UsaInstitut {
-    
+    public static void main(String[] args) throws Exception {
+        Institut Jaume = new Institut("INS Jaume Huguet");
+        Institut Narcis = new Institut("Narcis Oller");
+
+        Alumne David = new Alumne("David",22, "72179852X", "David@gmail.com");
+        Alumne Juan = new Alumne("Juan", 19, "72179852X", "Juan@gmail.com");
+        Alumne Pepe = new Alumne("Pepe", 21, "72179852X", "Pepe@gmail.com");
+        Alumne Manuel = new Alumne("Manuel", 19, "72179852X", "Manuel@gmail.com");
+        Alumne Jose = new Alumne("Jose", 19, "72179852X", "Pepe@gmail.com");
+        Alumne Pedro = new Alumne("Pedro", 20, "72179852X", "Pepe@gmail.com");
+        
+        Jaume.afegirAlumne(David);
+        Jaume.afegirAlumne(Pepe);
+        Jaume.afegirAlumne(Juan);
+
+        Narcis.afegirAlumne(Pedro);
+        Narcis.afegirAlumne(Manuel);
+        Narcis.afegirAlumne(Jose);
+
+        System.out.println("Alumnes de l'Institut " + Jaume.getNom() + ":");
+        for (int i = 0; i < Jaume.getNum(); i++) {
+            System.out.println(Jaume.getLlistaAlumnes()[i].getNom());
+        }
+
+        System.out.println("Alumnes de l'Institut " + Narcis.getNom() + ":");
+        for (int i = 0; i < Narcis.getNum(); i++) {
+            System.out.println(Narcis.getLlistaAlumnes()[i].getNom());
+        }
+
+    }
 }
